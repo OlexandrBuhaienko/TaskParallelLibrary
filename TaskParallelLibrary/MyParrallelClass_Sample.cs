4980e6c4b38a8a868ac647000ef44009e8fb8a8a
OlexandrBuhaienko <soulofdeveloper0911@gmail.com>
2023-10-05 01:04:39 +0300

    Reorganized and refactored code. Main turned into additional MyParallelClass_Sample method and invokes from the top level stated class Program. Same vars becomes properties and other method becomes private

---
+++ TaskParallelLibrary/MyParrallelClass_Sample.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskParallelLibrary
+{
+    internal class MyParrallelClass_Sample
+    {
+        public int TimesRepeat { get; private set; }
+        public int ThreadId { get; set; }
+        public MyParrallelClass_Sample()
+        {
+            ThreadId = Thread.CurrentThread.ManagedThreadId;
+            TimesRepeat = 10;
+        }
+        private void MyTask ()
+        {
+            
+            Console.WriteLine($"\nMyTask : Is runnin on {ThreadId} thread !");
+            for (int i = 0; i < TimesRepeat; i++)
+            {
+                Thread.Sleep(200);
+                Console.Write("+ ");
+            }
+            Console.WriteLine($"\nMyTask : Is ended on thread #{ThreadId}");
+        }
+        public void MainTask()
+        {
+            //Main працює синхронно
+            Console.WriteLine($"Main : Is running on thread #{ThreadId}");
+
+
+            Action action = new Action(MyTask);
+
+            Task task = new Task(action);
+
+            task.Start(); //Запуск методу асинхронно
+            for (int i = 0; i < TimesRepeat; i++)
+            {
+                Console.Write(". ");
+                Thread.Sleep(200);
+            }
+            Console.WriteLine($"Main : Ended on thread #{ThreadId}");
+        }
+    }
+}
--- TaskParallelLibrary/Program.cs
+++ TaskParallelLibrary/Program.cs
@@ -1,21 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using TaskParallelLibrary;
-
-const int timesRepeat = 10;
-int threadId = Thread.CurrentThread.ManagedThreadId;
-
-Console.WriteLine($"Main : Is running on thread #{threadId}");
-
-MyParrallelClass parrallelClass = new MyParrallelClass();
-
-Action action = new Action(parrallelClass.MyTask);
-
-Task task = new Task(action);
-
-task.Start(); //Запуск методу асинхронно
-for(int i = 0; i < timesRepeat; i++)
-{
-    Console.Write(". ");
-    Thread.Sleep(200);
-}
-Console.WriteLine($"Main : Ended on thread #{threadId}");
+MyParrallelClass_Sample parrallelClass = new MyParrallelClass_Sample();
+parrallelClass.MainTask();
