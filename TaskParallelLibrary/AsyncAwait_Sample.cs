abe808f6ad836545e6d591c0faccec35a0d8290f
OlexandrBuhaienko <soulofdeveloper0911@gmail.com>
2023-10-05 18:30:03 +0300

    Added AsyncAwait_Sample class that represents a simple logic using these key words and asynchonous operations. Also added two methods in Program class, to represent how to make asynchonous operaion from default operation

---
+++ TaskParallelLibrary/AsyncAwait_Sample.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskParallelLibrary
+{
+    internal class AsyncAwait_Sample
+    {
+        private void Operation()
+        {
+            Console.WriteLine($"Operation Thread Id : {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine("Begin");
+            Thread.Sleep(2000);
+            Console.WriteLine("End");
+        }
+        private async void OperationAsync()
+        {
+            // Id потоку співпадає з Id основного потоку. А це означає,
+            // що даний метод починає виконуватись в контексті первинного потоку.
+            Console.WriteLine($"OperationAsync (Part I) ThreadId : {Thread.CurrentThread.ManagedThreadId}");
+            Task task = new Task(Operation);
+            task.Start();
+            await task;
+            // Id потоку співпадає з Id вторинного потоку. А це означає,
+            // що даний метод починає виконуватись в контексті вторинного потоку.
+            Console.WriteLine($"OperationAsync (Part II) ThreadId : {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        public void MainOperation()
+        {
+            Console.WriteLine($"Main Thread Id : {Thread.CurrentThread.ManagedThreadId}");
+            OperationAsync();
+
+            Console.ReadKey();
+        }
+    }
+}
--- TaskParallelLibrary/Program.cs
+++ TaskParallelLibrary/Program.cs
@@ -2,3 +2,25 @@
 using TaskParallelLibrary;
 MyParrallelClass_Sample parrallelClass = new MyParrallelClass_Sample();
 parrallelClass.MainTask();
+Main();
+Console.ReadKey();
+
+
+static async Task Main()
+{
+    Console.WriteLine($"Main Thread Id: {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+
+    await RunAsyncOperation();
+
+    Console.WriteLine("Main End");
+}
+
+static async Task RunAsyncOperation()
+{
+    Console.WriteLine($"AsyncOperation Thread Id: {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+
+    // Simulate an asynchronous operation with await
+    await Task.Delay(1000);
+
+    Console.WriteLine("AsyncOperation End");
+}
